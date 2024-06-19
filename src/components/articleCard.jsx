77721201a6db2cadc98a4b4afd58b549302f2bf2
mangianees <mangianees@gmail.com>
2024-06-19 11:04:40 +0100

    Task 5 completed

--- src/components/articleCard.jsx
+++ src/components/articleCard.jsx
@@ -5,7 +5,7 @@ import Card from 'react-bootstrap/Card';
 
 export const ArticleCard=({article})=>{
     
-// console.log(article,'recieved at card');
+
 const[dataOfArticle,setDataOfArticle] = useState();
 const [isLoading,setIsLoading] = useState(true);
     
@@ -14,40 +14,34 @@ const [isLoading,setIsLoading] = useState(true);
         setDataOfArticle(article)
         setIsLoading(false)
     },[])
-
-
-    const handleIndividualCard = ()=>{
-        console.log("Clicked");
-    }
     
     return isLoading ? (<p>Data Loading</p>):(
-        <Card>
-    <Link to={`/${dataOfArticle.title}`} onClick={handleIndividualCard}>
-      <Card.Body >
-        <Card.Title>Author: {dataOfArticle.author}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">Topic: {dataOfArticle.topic}</Card.Subtitle>
-        <Card.Img variant="top" src={dataOfArticle.article_img_url} className='imageCard'/>
-        <Card.Text>
-          {dataOfArticle.title}
-        </Card.Text>
-        <p>Created at: {dataOfArticle.created_at}</p>
-        
-        <dir>
-        <span className="p-3 mb-2 bg-dark text-white">
-        <Button variant="dark" className='cardButtons'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
-        </svg></Button>
-        Votes: {dataOfArticle.votes}
-        <Button variant="dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
-        <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
-</svg></Button>
-        </span>
-        </dir>
-        <br /><br />
-        <Button variant="dark">Comments: {dataOfArticle.comment_count}</Button>
-        
-        
-      </Card.Body>
+        <Card className='cardBody'>
+          <Link to={`/articles/${dataOfArticle.article_id}`}>
+            <Card.Body className='cardBody'>
+              <Card.Title>Author: {dataOfArticle.author} Title: {dataOfArticle.title}</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">Topic: {dataOfArticle.topic}</Card.Subtitle>
+              <Card.Img variant="top" src={dataOfArticle.article_img_url} className='imageCard'/>
+              <Card.Text>
+              {dataOfArticle.body}
+              </Card.Text>
+              <p>Created at: {dataOfArticle.created_at}</p>
+            
+              <span className="p-3 mb-2 bg-dark text-white">
+              <Button variant="dark" className='cardButtons'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-up-short" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M8 12a.5.5 0 0 0 .5-.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 .5.5"/>
+              </svg></Button>
+              Votes: {dataOfArticle.votes}
+              <Button variant="dark"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down" viewBox="0 0 16 16">
+              <path fillRule="evenodd" d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1"/>
+      </svg></Button>
+              </span>
+            
+            <br /><br />
+            <Button variant="dark">Comments: {dataOfArticle.comment_count}</Button>
+            
+            
+          </Card.Body>
       </Link>
     </Card>
 
--- src/components/articles.jsx
+++ src/components/articles.jsx
@@ -1,27 +1,45 @@
-
 import { useState,useEffect} from "react";
 import { ArticleCard } from "./articleCard";
+import { useParams } from "react-router-dom";
+import { getNcArticleById } from "../assets/utilities/api";
+
 
 export const Articles =({articles})=>{
+
     const [isLoading,setIsLoading]=useState(true)
     const [allArticles,setAllArticles] = useState('');
-    useEffect(()=>{
-        setIsLoading(true)
-        setAllArticles(articles)
-        setIsLoading(false)
-    },[])
+    const {article_id} = useParams();
+    
 
+    if(article_id){
+              useEffect(()=>{
+              setIsLoading(true)
+              setAllArticles('')
+              getNcArticleById(article_id).then((articlesFromApi)=>{
+              setAllArticles(articlesFromApi)  
+              })
+              setIsLoading(false)
+            },[article_id])
+    }else{
+        useEffect(()=>{
+            setIsLoading(true)
+            setAllArticles('')
+            setAllArticles(articles)
+            setIsLoading(false)
+        },[article_id])
+
+    }
+    
     return isLoading?(<p>Loading.........</p>):
                       (         
         <>
         <div className="body">
-            {   
-                allArticles.map((article) =>{
-                    return (<>
-                    <ArticleCard article = {article}/>
-                    </>)
-                })
-
+            {   !allArticles ? null :allArticles.map((article) =>{
+                                    return (<>
+                                    <ArticleCard article = {article}/>
+                                    </>)
+                                    })
+            
                 
             }
             </div>
